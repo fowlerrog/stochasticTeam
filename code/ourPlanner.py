60869e39d78c7905fe709c6e8352c86b76f7e803
fowlerrog <fowler.ro@northeastern.edu>
2025-10-10 15:57:55 -0400

    reordering ourPlanner by call order

--- code/ourPlanner.py
+++ code/ourPlanner.py
@@ -10,186 +10,6 @@ from gtspUtils import *
 
 INF = 999999
 
-def buildt_GTSP_matrix(mapping_to_release, mapping_to_collect, points, cycles, start_point, end_point, collect_to_cycle_times, dim, params):
-    """
-    Builds a distance matrix and cluster grouping list for UGV's GTSP solution
-    """
-    UGV_SPEED = params["UGV_SPEEDS"]
-    CHARGE_RATE = params["CHARGE_RATE"]
-
-    matrix = np.ones((dim, dim)) * INF
-
-    first_release_point = points[cycles[0][0]]
-
-    matrix [0,mapping_to_release[cycles[0][0]]] = euclidean(first_release_point, start_point) / UGV_SPEED
-    matrix [mapping_to_release[cycles[0][0]], 0] = euclidean(first_release_point, start_point) / UGV_SPEED
-
-    matrix[dim-1, 0] = 0
-    matrix[0, dim-1] = 0
-    matrix[dim-2, dim-1] = 0
-    matrix[dim-1, dim-2] = 0
-
-    clusters = [[0],[dim-1],[dim-2]]
-
-    for cycle_index in range(0, len(cycles)):
-        cycle = cycles[cycle_index]
-        collect_times_dict = collect_to_cycle_times[cycle_index]
-        release_idx = cycle[0]
-        clusters.append([mapping_to_release[release_idx]])
-        collect_cluster = []
-        for collect_idx, cycle_time in collect_times_dict.items():
-            collect_graph_idx = mapping_to_collect[collect_idx]
-            collect_point = points[collect_idx]
-            collect_cluster.append(collect_graph_idx)
-            matrix[collect_graph_idx, mapping_to_release[release_idx]] = cycle_time
-            matrix[mapping_to_release[release_idx], collect_graph_idx] = cycle_time
-
-            if cycle_index < len(cycles) - 1:
-                next_release_idx = cycles[cycle_index + 1][0]
-                next_release_graph_idx = mapping_to_release[next_release_idx]
-                next_release_point = points[next_release_idx]
-                matrix[collect_graph_idx, next_release_graph_idx] = max(euclidean(collect_point, next_release_point) / UGV_SPEED, cycle_time * CHARGE_RATE)
-                matrix[next_release_graph_idx, collect_graph_idx] = max(euclidean(collect_point, next_release_point) / UGV_SPEED, cycle_time * CHARGE_RATE)
-        clusters.append(collect_cluster)
-    
-    for collect_idx in collect_to_cycle_times[-1].keys():
-        matrix[mapping_to_collect[collect_idx], dim-2] = euclidean(points[collect_idx], end_point) / UGV_SPEED
-        matrix[dim-2, mapping_to_collect[collect_idx]] = euclidean(points[collect_idx], end_point) / UGV_SPEED
-    
-    matrix *= 100
-    matrix = matrix.astype(int)
-    print(f"Clusters: {clusters}")
-    return matrix, clusters
-
-def solve_gtsp_with_release_collect(points, cycles, start_point, end_point, collect_to_cycle_times, params):
-    """
-    Solves the UGV's GTSP problem: chooses a release and collect point for each UAV cycle in points
-    """
-    KSI = params["KSI"]
-    runFolder = params["RUN_FOLDER"]
-
-    points = np.array(points)[:, :2]
-    graph_index = 1
-    mapping_to_release = {}
-    mapping_to_collect = {}
-    mapping_to_points = {0: start_point}
-    # TODO: Use graphx
-    for cycle, collect_times_dict in zip(cycles, collect_to_cycle_times):
-        release_idx = cycle[0]
-        mapping_to_release[release_idx] = graph_index
-        mapping_to_points[graph_index] = points[release_idx]
-        graph_index += 1
-        for collect_idx in collect_times_dict.keys():
-            mapping_to_collect[collect_idx] = graph_index
-            mapping_to_points[graph_index] = points[collect_idx]
-            graph_index += 1
-
-    mapping_to_points[graph_index] = end_point
-    mapping_to_points[graph_index+1] = params["DUMMY_POINT"]  # Dummy point
-    graph_index += 2
-    dim = graph_index
-
-    print(f"Mapping to release:")
-    pprint(mapping_to_release)
-    print(f"Mapping to collect:")
-    pprint(mapping_to_collect)
-
-    distance_matrix, clusters = buildt_GTSP_matrix(mapping_to_release, mapping_to_collect, points, cycles, start_point, end_point, collect_to_cycle_times, dim, params)
-    gtsp_input_path = os.path.join(runFolder, "GTSP_input.gtsp")
-    gtsp_output_path = os.path.join(runFolder, "GTSP_output.txt")
-    write_gtsp_file(dim, clusters, distance_matrix, gtsp_input_path)
-
-    max_run_time = KSI * len(points) ** 3
-    subprocess.run([
-        "julia", "-e",
-        f'using GLNS; solver("{escape_path(gtsp_input_path)}", output="{escape_path(gtsp_output_path)}", max_time={max_run_time}); solver("{escape_path(gtsp_input_path)}", output="{escape_path(gtsp_output_path)}", max_time={max_run_time})'
-    ])
-
-    tour, solver_time, total_cost = read_glns_output(gtsp_output_path)
-    print(f"Tour: {tour}")
-
-    path = tour_to_path(tour, start_idx=0, dummy_idx=dim-1)
-    print(f"Path: {path}")
-
-    return {
-        "raw_tour": tour,
-        "path": path,
-        "gtsp_file": gtsp_input_path,
-        "output_file": gtsp_output_path,
-        "solver_time": solver_time,
-        "total_cost": total_cost,
-        "clusters": clusters,
-        "mapping_to_points": mapping_to_points
-    }
-
-def compute_all_cycle_times(cycles, points, params):
-    """
-    Computes UAV times for all cycles and all possible collect points in each cycle.
-    Adds fixed takeoff and landing time to each.
-    Checks feasibility against UAV battery and UGV travel limits.
-    Returns two lists:
-        all_times_list: list of dicts mapping all collect_idx to UAV time
-        feasible_times_list: list of dicts mapping only feasible collect_idx to UAV time
-    """
-    UAV_SPEED = params["UAV_SPEED"]
-    UGV_SPEED = params["UGV_SPEEDS"]
-    TAKEOFF_LANDING_TIME = params["TAKEOFF_LANDING_TIME"]
-    UAV_BATTERY_TIME = params["UAV_BATTERY_TIME"]
-
-    points = np.array(points)
- 
-    collect_points = []
-    for i, cycle in enumerate(cycles):
-        collect_to_cycle_time = {}
-        #TODO first and last should be different. 
-        travel_dist = sum(np.linalg.norm(points[cycle[k+1]] - points[cycle[k]]) for k in range(len(cycle) - 1))
-
-        for collect_idx in cycle:
-            collect_point = points[collect_idx]
-            if collect_idx == cycle[-1]:
-                total_dist = travel_dist
-            else:
-                last_point = points[cycle[-1]]
-                #TODO project the below collect
-                extra_dist = np.linalg.norm(collect_point - last_point)
-                total_dist = travel_dist + extra_dist
-
-            time_sec = total_dist / UAV_SPEED + TAKEOFF_LANDING_TIME
-            if time_sec > UAV_BATTERY_TIME:
-                continue
-
-            ugv_release = points[cycle[0]]
-            ugv_collect = points[collect_idx]
-            #TODO below should be projected
-            ugv_dist = np.linalg.norm(ugv_collect - ugv_release)
-            ugv_time = ugv_dist / UGV_SPEED
-
-
-            #
-            # # #
-            # # # # NEW CONSTRAINT WILL CHECK ONLY THE MAX BATTERY
-            if ugv_time > UAV_BATTERY_TIME:
-                continue
-            # if ugv_time > time_sec:
-            #     continue
-            # # #
-            # #
-            #
-
-
-            collect_to_cycle_time[collect_idx] = time_sec
-
-            #TODO
-            #total_dist = toal dis - return_dist + last_uav_treavel_actual_dist
-
-
-        collect_points.append(collect_to_cycle_time)
-    
-    print("Collect points:")
-    pprint(collect_points)
-
-    return collect_points
-
 def solve_tsp_with_fixed_start_end(points, start_point, end_point):
     # Step 1: Find the closest points to START_POINT and END_POINT
     start_index = find_closest_point(points, start_point)
@@ -365,3 +185,182 @@ def create_cycles_CAHIT(tsp_tour, params):
     pprint(cycles)
     print(f"Cycle times CAHIT = {cycle_times}")
     return cycles
+
+def compute_all_cycle_times(cycles, points, params):
+    """
+    Computes UAV times for all cycles and all possible collect points in each cycle.
+    Adds fixed takeoff and landing time to each.
+    Checks feasibility against UAV battery and UGV travel limits.
+    Returns one list:
+        collect_points: list of dicts mapping only feasible collect_idx to UAV time
+    """
+    UAV_SPEED = params["UAV_SPEED"]
+    UGV_SPEED = params["UGV_SPEEDS"]
+    TAKEOFF_LANDING_TIME = params["TAKEOFF_LANDING_TIME"]
+    UAV_BATTERY_TIME = params["UAV_BATTERY_TIME"]
+
+    points = np.array(points)
+ 
+    collect_points = []
+    for _, cycle in enumerate(cycles):
+        collect_to_cycle_time = {}
+        #TODO first and last should be different. 
+        travel_dist = sum(np.linalg.norm(points[cycle[k+1]] - points[cycle[k]]) for k in range(len(cycle) - 1))
+
+        for collect_idx in cycle:
+            collect_point = points[collect_idx]
+            if collect_idx == cycle[-1]:
+                total_dist = travel_dist
+            else:
+                last_point = points[cycle[-1]]
+                #TODO project the below collect
+                extra_dist = np.linalg.norm(collect_point - last_point)
+                total_dist = travel_dist + extra_dist
+
+            time_sec = total_dist / UAV_SPEED + TAKEOFF_LANDING_TIME
+            if time_sec > UAV_BATTERY_TIME:
+                continue
+
+            ugv_release = points[cycle[0]]
+            ugv_collect = points[collect_idx]
+            #TODO below should be projected
+            ugv_dist = np.linalg.norm(ugv_collect - ugv_release)
+            ugv_time = ugv_dist / UGV_SPEED
+
+
+            #
+            # # #
+            # # # # NEW CONSTRAINT WILL CHECK ONLY THE MAX BATTERY
+            if ugv_time > UAV_BATTERY_TIME:
+                continue
+            # if ugv_time > time_sec:
+            #     continue
+            # # #
+            # #
+            #
+
+
+            collect_to_cycle_time[collect_idx] = time_sec
+
+            #TODO
+            #total_dist = toal dis - return_dist + last_uav_treavel_actual_dist
+
+
+        collect_points.append(collect_to_cycle_time)
+    
+    print("Collect points:")
+    pprint(collect_points)
+
+    return collect_points
+
+def buildt_GTSP_matrix(mapping_to_release, mapping_to_collect, points, cycles, start_point, end_point, collect_to_cycle_times, dim, params):
+    """
+    Builds a distance matrix and cluster grouping list for UGV's GTSP solution
+    """
+    UGV_SPEED = params["UGV_SPEEDS"]
+    CHARGE_RATE = params["CHARGE_RATE"]
+
+    matrix = np.ones((dim, dim)) * INF
+
+    first_release_point = points[cycles[0][0]]
+
+    matrix [0,mapping_to_release[cycles[0][0]]] = euclidean(first_release_point, start_point) / UGV_SPEED
+    matrix [mapping_to_release[cycles[0][0]], 0] = euclidean(first_release_point, start_point) / UGV_SPEED
+
+    matrix[dim-1, 0] = 0
+    matrix[0, dim-1] = 0
+    matrix[dim-2, dim-1] = 0
+    matrix[dim-1, dim-2] = 0
+
+    clusters = [[0],[dim-1],[dim-2]]
+
+    for cycle_index in range(0, len(cycles)):
+        cycle = cycles[cycle_index]
+        collect_times_dict = collect_to_cycle_times[cycle_index]
+        release_idx = cycle[0]
+        clusters.append([mapping_to_release[release_idx]])
+        collect_cluster = []
+        for collect_idx, cycle_time in collect_times_dict.items():
+            collect_graph_idx = mapping_to_collect[collect_idx]
+            collect_point = points[collect_idx]
+            collect_cluster.append(collect_graph_idx)
+            matrix[collect_graph_idx, mapping_to_release[release_idx]] = cycle_time
+            matrix[mapping_to_release[release_idx], collect_graph_idx] = cycle_time
+
+            if cycle_index < len(cycles) - 1:
+                next_release_idx = cycles[cycle_index + 1][0]
+                next_release_graph_idx = mapping_to_release[next_release_idx]
+                next_release_point = points[next_release_idx]
+                matrix[collect_graph_idx, next_release_graph_idx] = max(euclidean(collect_point, next_release_point) / UGV_SPEED, cycle_time * CHARGE_RATE)
+                matrix[next_release_graph_idx, collect_graph_idx] = max(euclidean(collect_point, next_release_point) / UGV_SPEED, cycle_time * CHARGE_RATE)
+        clusters.append(collect_cluster)
+    
+    for collect_idx in collect_to_cycle_times[-1].keys():
+        matrix[mapping_to_collect[collect_idx], dim-2] = euclidean(points[collect_idx], end_point) / UGV_SPEED
+        matrix[dim-2, mapping_to_collect[collect_idx]] = euclidean(points[collect_idx], end_point) / UGV_SPEED
+    
+    matrix *= 100
+    matrix = matrix.astype(int)
+    print(f"Clusters: {clusters}")
+    return matrix, clusters
+
+def solve_gtsp_with_release_collect(points, cycles, start_point, end_point, collect_to_cycle_times, params):
+    """
+    Solves the UGV's GTSP problem: chooses a release and collect point for each UAV cycle in points
+    """
+    KSI = params["KSI"]
+    runFolder = params["RUN_FOLDER"]
+
+    points = np.array(points)[:, :2]
+    graph_index = 1
+    mapping_to_release = {}
+    mapping_to_collect = {}
+    mapping_to_points = {0: start_point}
+    # TODO: Use graphx
+    for cycle, collect_times_dict in zip(cycles, collect_to_cycle_times):
+        release_idx = cycle[0]
+        mapping_to_release[release_idx] = graph_index
+        mapping_to_points[graph_index] = points[release_idx]
+        graph_index += 1
+        for collect_idx in collect_times_dict.keys():
+            mapping_to_collect[collect_idx] = graph_index
+            mapping_to_points[graph_index] = points[collect_idx]
+            graph_index += 1
+
+    mapping_to_points[graph_index] = end_point
+    mapping_to_points[graph_index+1] = params["DUMMY_POINT"]  # Dummy point
+    graph_index += 2
+    dim = graph_index
+
+    print(f"Mapping to release:")
+    pprint(mapping_to_release)
+    print(f"Mapping to collect:")
+    pprint(mapping_to_collect)
+
+    distance_matrix, clusters = buildt_GTSP_matrix(mapping_to_release, mapping_to_collect, points, cycles, start_point, end_point, collect_to_cycle_times, dim, params)
+    gtsp_input_path = os.path.join(runFolder, "GTSP_input.gtsp")
+    gtsp_output_path = os.path.join(runFolder, "GTSP_output.txt")
+    write_gtsp_file(dim, clusters, distance_matrix, gtsp_input_path)
+
+    max_run_time = KSI * len(points) ** 3
+    subprocess.run([
+        "julia", "-e",
+        f'using GLNS; solver("{escape_path(gtsp_input_path)}", output="{escape_path(gtsp_output_path)}", max_time={max_run_time}); solver("{escape_path(gtsp_input_path)}", output="{escape_path(gtsp_output_path)}", max_time={max_run_time})'
+    ])
+
+    tour, solver_time, total_cost = read_glns_output(gtsp_output_path)
+    print(f"Tour: {tour}")
+
+    path = tour_to_path(tour, start_idx=0, dummy_idx=dim-1)
+    print(f"Path: {path}")
+
+    return {
+        "raw_tour": tour,
+        "path": path,
+        "gtsp_file": gtsp_input_path,
+        "output_file": gtsp_output_path,
+        "solver_time": solver_time,
+        "total_cost": total_cost,
+        "clusters": clusters,
+        "mapping_to_points": mapping_to_points
+    }
