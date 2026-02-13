103af20236dc55363341a4ce2302d1ed8b38ed8f
fowlerrog <fowler.ro@northeastern.edu>
2026-02-13 10:50:04 -0500

    Working roscopter sim launch

--- gazebo/launch.sh
+++
@@ -1,3 +0,0 @@
-
-source /opt/ros/jazzy/setup.bash
-source /opt/ros/jazzy/share/mrs_multirotor_simulator/local_setup.bash
--- gazebo/setup.sh
+++ gazebo/setup.sh
@@ -1,14 +1,15 @@
 
 # puts us in the python virtual environment, and sources ros
 
-SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
-echo "This script is at $SCRIPT_DIR"
+SCRIPT=$(realpath "${BASH_SOURCE[0]}")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+echo "    This script is at $SCRIPT_DIR"
 
-echo "Entering python venv"
-source $SCRIPT_DIR/../.venv/bin/activate
+#echo "Entering python venv @ $SCRIPT_DIR/../.venv/bin/activate"
+#source $SCRIPT_DIR/../.venv/bin/activate
 
-echo "Sourcing ROS2"
+echo "Sourcing ROS2 @ /opt/ros/jazzy/setup.bash"
 source /opt/ros/jazzy/setup.bash
 
-echo "Sourcing ros_ws"
+echo "Sourcing ros_ws @ $SCRIPT_DIR/ros_ws/install/setup.bash"
 source $SCRIPT_DIR/ros_ws/install/setup.bash
